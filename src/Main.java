a2452da92522fe9a9d8837e5b7ad7105cca1ab29
Ivy Li <ivyl68@nycstudents.net>
2025-11-20 15:34:18 -0500

    aaa

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args) {
+        String info;
+        Scoreboard game = new Scoreboard("Red", "Blue");
+        info = game.getScore();
+    }
+}
--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+public class Scoreboard
+{
+    public int team1Score;
+    public int team2Score;
+    public String activeTeam;
+    public String firstTeam;
+    public String secondTeam;
+    public Scoreboard(String team1, String team2)
+    {
+        firstTeam = team1;
+        secondTeam = team2;
+        activeTeam = team1;
+    }
+    public void recordPlay(int score)
+    {
+        if (score >= 1 && activeTeam.equals(firstTeam))
+        {
+            team1Score += score;
+        }
+        if (score == 0 && activeTeam.equals(firstTeam))
+        {
+            activeTeam = secondTeam;
+        }
+        if (score >= 1 && activeTeam.equals(secondTeam)) {
+            team2Score += score;
+        }
+        if (score == 0 && activeTeam.equals(secondTeam)) {
+            activeTeam = firstTeam;
+        }
 
-public class Scoreboard {
+    }
+    public void String  getScore()
+    {
+        System.out.println(team1Score + "-" + team2Score + "-" + activeTeam);
+    }
+
 }
